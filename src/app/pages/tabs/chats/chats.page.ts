bf76edd028667c14d88ebd71f40d4799668a86e9
haroldlaw <g00438467@atu.ie>
2025-04-14 14:19:49 +0100

    chat page completed

--- src/app/pages/tabs/chats/chats.page.ts
+++ src/app/pages/tabs/chats/chats.page.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonItem, IonTitle, IonToolbar, IonAvatar, IonImg, IonList, IonLabel, IonIcon, IonButton, IonButtons } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addCircle } from 'ionicons/icons';
 
 @Component({
   selector: 'app-chats',
   templateUrl: './chats.page.html',
   styleUrls: ['./chats.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonImg, IonAvatar, IonLabel, IonIcon, IonButtons, IonButton]
 })
 export class ChatsPage implements OnInit {
+  chats = Array(10)
 
-  constructor() { }
+  constructor() { 
+    addIcons({
+      addCircle
+    })
+  }
 
   ngOnInit() {
   }
--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -29,4 +29,9 @@ export const routes: Routes = [
             },
         ],
     },
+    {
+        path: '',
+        redirectTo: '/tabs/chats',
+        pathMatch: 'full',
+    }
 ];
