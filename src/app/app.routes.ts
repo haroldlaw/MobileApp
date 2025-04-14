d5e7e386db3cbc7d42f3d9ff9fcbe80044a9ce86
haroldlaw <g00438467@atu.ie>
2025-04-14 14:24:43 +0100

    login and signup page generated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,19 @@ export const routes: Routes = [
   },
   {
     path: 'tabs',
-    loadChildren: () => import('./pages/tabs/tabs.routes').then( m => m.routes)
+    loadChildren: () => import('./pages/tabs/tabs.routes').then(m => m.routes)
+  },
+  {
+    path: 'login',
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('./pages/login/login.page').then(m => m.LoginPage)
+      },
+      {
+        path: 'signup',
+        loadComponent: () => import('./pages/login/signup/signup.page').then(m => m.SignupPage)
+      }
+    ]
   },
 ];
